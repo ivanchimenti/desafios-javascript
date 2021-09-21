f9e221fcbfe6806f1ecd04d80a83bd161b2ba71c
ivanchimenti <ivanchimenti@gmail.com>
2021-09-21 12:04:25 -0300

    Desafio 1

---
+++ Clase-1-Conceptos-Generales-Sintaxis-y-Variables/Desafio-1/app.js
@@ -0,0 +1,21 @@
+let nombre = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+
+alert(`Bienvenido ${nombre} ${apellido}`)
+console.log("Bienvenido a la consola " + nombre +" "+ apellido)
+
+alert("A continuacion usted va a ingresar dos números a elección para realizar una suma")
+
+let nro1 = parseInt(prompt("Ingrese el primer número"))
+console.log("El primer número ingresado fue " + nro1)
+
+let nro2 = parseInt(prompt("Ingrese el segundo número"))
+console.log("El segundo número ingresado fue " + nro2)
+
+alert(`La operación a realizar es ${nro1} + ${nro2}`)
+let suma = nro1 + nro2
+alert("El resutado de la suma es: " + suma)
+console.log("El resultado de la suma es " + suma)
+
+alert("Que tengas un buen día " + nombre + " =D")
+console.log(`Muchas gracias ${nombre}, volvé pronto`)
