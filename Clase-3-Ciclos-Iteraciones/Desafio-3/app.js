a1f9ed65e1a4867e6b264b3b5017f98759963cff
ivanchimenti <ivanchimenti@gmail.com>
2021-09-26 12:58:14 -0300

    Desafio 3 Correcion

--- Clase-3-Ciclos-Iteraciones/Desafio-3/app.js
+++ Clase-3-Ciclos-Iteraciones/Desafio-3/app.js
@@ -1,12 +1,14 @@
 alert("Ingrese los datos de los alumnos mayores de 18 años (DNI menor o igual a 46 millones, finaliza con DNI = 0 o con un DNI mayor a 46 millones)")
 let dni = parseInt(prompt("Ingrese el DNI del alumno"))
+
+document.write("Los datos ingresados de los alumnos son:<br/><br/>")
+
 while ((dni<=46000000)&&(dni!=0)) {
     let nombreAlumno = prompt("Ingrese el nombre")
     let apellidoAlumno = prompt("Ingrese el apellido")
     let fechaNacimiento = parseInt(prompt("Ingrese fecha de nacimiento en formato DDMMAAAA"))
     let carrera = prompt("Ingrese la carrera que estudia el alumno")
 
-    document.write("Los datos del alumno son:<br/><br/>")
     document.write(`Nombre: ${nombreAlumno}<br/>
                     Apellido: ${apellidoAlumno}<br/>
                     Fecha de Nacimiento: ${fechaNacimiento}<br/>
