1b21e88070cc6079d2b55978eae07fdc6aa4de75
ivanchimenti <ivanchimenti@gmail.com>
2021-09-26 01:33:00 -0300

    Correccion Desafio 3 - Le agregue el LET a las variables porque me lo habia olvidado pero sin el LET me funciono igual

--- Clase-3-Ciclos-Iteraciones/Desafio-3/app.js
+++ Clase-3-Ciclos-Iteraciones/Desafio-3/app.js
@@ -1,10 +1,10 @@
 alert("Ingrese los datos de los alumnos mayores de 18 años (DNI menor o igual a 46 millones, finaliza con DNI = 0 o con un DNI mayor a 46 millones)")
-dni = parseInt(prompt("Ingrese el DNI del alumno"))
+let dni = parseInt(prompt("Ingrese el DNI del alumno"))
 while ((dni<=46000000)&&(dni!=0)) {
-    nombreAlumno = prompt("Ingrese el nombre")
-    apellidoAlumno = prompt("Ingrese el apellido")
-    fechaNacimiento = parseInt(prompt("Ingrese fecha de nacimiento en formato DDMMAAAA"))
-    carrera = prompt("Ingrese la carrera que estudia el alumno")
+    let nombreAlumno = prompt("Ingrese el nombre")
+    let apellidoAlumno = prompt("Ingrese el apellido")
+    let fechaNacimiento = parseInt(prompt("Ingrese fecha de nacimiento en formato DDMMAAAA"))
+    let carrera = prompt("Ingrese la carrera que estudia el alumno")
 
     document.write("Los datos del alumno son:<br/><br/>")
     document.write(`Nombre: ${nombreAlumno}<br/>
